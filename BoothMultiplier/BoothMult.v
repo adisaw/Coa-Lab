3eaa7f233782402b375d599315107e2365fc8f32
adisaw <46715774+adisaw@users.noreply.github.com>
2020-01-15 23:09:00 +0530

    Add files via upload

---
+++ BoothMultiplier/BoothMult.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:35:09 09/02/2019 
+// Design Name: 
+// Module Name:    BoothMult 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module BoothMult(
+    input clk,
+    input rst,
+    input load,
+    output reg slow_clk,
+    input [5:0]a,
+    input [5:0]b,
+	 output reg [11:0]product
+    );
+
+	reg [5:0]la,lb;
+	reg [2:0]counter=0;
+	reg [11:0] temp;	
+	reg [32:0]mcounter=0;
+	reg [6:0]Q=0;
+	reg [5:0]A=0;
+
+	always @(posedge clk)
+    begin
+        mcounter = (mcounter>=8000000)?0:mcounter+1;
+        slow_clk = (mcounter < 4000000)?1'b0:1'b1;
+    end
+always @(posedge slow_clk)
+begin
+	if(rst==1)
+	begin
+		product=0;
+		counter=0;
+	end
+	else if(load==1)
+	begin
+		product=0;
+		counter=0;
+		la=a[5:0]; 
+		lb=b[5:0];
+		A=0;
+		Q={la[5:0],1'b0};
+	end
+	else if(counter<=5)
+	begin	
+		if( Q[1]==0 && Q[0]==1)
+		begin
+			A=A+lb;
+		end
+		else if( Q[1]==1 && Q[0]==0)
+		begin 
+			A=A-lb;
+		end
+		Q={A[0],Q[6:1]};
+		A={A[5],A[5:1]};
+		product={A,Q[6:1]};
+		counter=counter+1;
+	end
+end
+endmodule
---
+++ BoothMultiplier/tb.v
@@ -0,0 +1,98 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:49:09 09/02/2019
+// Design Name:   BoothMult
+// Module Name:   E:/group19Verilog/Assgn_7_Grp_19/BoothMultiplier/tb.v
+// Project Name:  BoothMultiplier
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: BoothMult
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg load;
+	reg [5:0] a;
+	reg [5:0] b;
+
+	// Outputs
+	wire slow_clk;
+	wire [11:0] product;
+
+
+	// Instantiate the Unit Under Test (UUT)
+	BoothMult uut (
+		.clk(clk), 
+		.rst(rst), 
+		.load(load), 
+		.slow_clk(slow_clk), 
+		.a(a), 
+		.b(b), 
+		.product(product)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		load = 0;
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		clk=1;
+		load=1;
+		a=6'b000001;
+		b=6'b000011;
+		#5;
+		load=0;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		clk=0;
+		#10
+		clk=1;
+		#10;
+		
+		
+		
+	end
+      
+endmodule
+
